00e962c9efa021321aa31b9c174d080f15d50325
Jimin Baek <qorwlals0012@naver.com>
2024-03-25 16:31:34 +0900

    first commit

--- script/mypage.js
+++ script/mypage.js
@@ -1,13 +1,18 @@
 //로그아웃 모달
 const logoutButton = document.querySelector('.logout');
+
+//첫번째 모달창
 const modal = document.getElementById('myModal');
 const confirmButton = document.getElementById('confirm');
-const modal2 = document.getElementById('myModal2');
 const cancelButton= document.getElementById('cancel');
-const loginButton = document.querySelector('.login');
 
+//두번째 모달창
+const modal2 = document.getElementById('myModal2');
+const loginButton = document.getElementById('login');
+
+//이벤트
 logoutButton.addEventListener('click', ()=>{
-  modal.style.display = 'block';
+  modal.style.display='block';
 });
 
 cancelButton.addEventListener('click', ()=>{
@@ -19,19 +24,16 @@ confirmButton.addEventListener('click', () => {
   modal2.style.display = 'block'; // 두 번째 모달 표시
 });
 
-// 두 번째 모달에서 취소 버튼 클릭 이벤트
-cancelButton2.addEventListener('click', () => {
-  modal2.style.display = 'none';
-});
-
-// 두 번째 모달에서 확인 버튼 클릭 이벤트
-confirmButton2.addEventListener('click', () => {
-  // 두 번째 모달에서 확인 버튼을 클릭한 후의 동작을 여기에 추가
-  modal2.style.display = 'none';
-});
-
 window.addEventListener('click',(event)=>{
   if(event.target===modal) {
     modal.style.display = 'none';
   }
+  
+  if(event.target===modal2) {
+    modal2.style.display = 'none';
+  }
+});
+
+loginButton.addEventListener('click', ()=>{
+  modal2.style.display = 'none';
 });
